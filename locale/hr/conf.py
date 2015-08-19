9bc5a568aef1819770d2cd9b88f594413a1e0105
Bruno Skvorc <bruno@skvorc.me>
2015-08-19 05:23:27 +0000

    Fixing RTD

--- locale/hr/conf.py
+++ locale/hr/conf.py
@@ -4,7 +4,6 @@ import os
 PROJECT_PATH = os.path.dirname(os.path.realpath('%s/../..' % __file__))
 
 sys.path.append(PROJECT_PATH)
-sys.path.append(os.path.relpath("../../"))
 
 from conf import *
 
