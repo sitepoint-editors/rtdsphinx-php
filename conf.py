bdf4faaa67a17c78edfe62cde4773c7869308f8a
Bruno Skvorc <bruno@skvorc.me>
2015-09-03 09:30:16 +0000

    Trying to fix custom style for RTD

--- conf.py
+++ conf.py
@@ -289,13 +289,13 @@ locale_dirs = ['locale/']
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-    # Override default css to get a larger width for local build
-    html_style = "css/my_theme.css"
-else:
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# Override default css to get a larger width for local build
+html_style = "css/my_theme.css"
+
+if on_rtd:
     # Override default css to get a larger width for ReadTheDoc build
     html_context = {
         'css_files': [
